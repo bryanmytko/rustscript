4f378e03e118317d9db6728df2a8b741e0557d54
Bryan Mytko <bryanmytko@gmail.com>
2016-03-01 04:44:40 -0500

    Add Token::Whitespace

--- src/lex.rs
+++ src/lex.rs
@@ -2,11 +2,10 @@ extern crate regex;
 
 use std::io::prelude::*;
 use std::fs::File;
+use std::path::Path;
 use std::env;
 use regex::Regex;
 use std::iter::Peekable;
-
-//let mut file = try!(File::open(""))
 
 #[derive(Eq, PartialEq, Debug, Clone)]
 pub enum Token {
@@ -16,6 +15,7 @@ pub enum Token {
     Implies(String),
     Variable(String),
     Atom(String),
+    Whitespace(String),
 }
 
 pub struct Tokenizer<I: Iterator<Item=char>> {
@@ -34,30 +34,41 @@ impl<I: Iterator<Item=char>> Iterator for Tokenizer<I> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Token> {
-        match *self.iter.peek().unwrap_or(&'.') {
+        match *self.iter.peek().unwrap_or(&'♥') {
+            // @TODO this is wrong; overflows buffer. Use take(1)
             '('       => Some(Token::DelimL(self.iter.by_ref().collect())),
-            // ')'       => {Some(Token::DelimR(self.iter.by_ref().collect()))},
-            // ','       => {Some(Token::Separator(self.iter.by_ref().collect()))},
-            // '='       => {Some(Token::Implies(self.iter.by_ref().collect()))},
+            // ')'    => {Some(Token::DelimR(self.iter.by_ref().collect()))},
+            // ','    => {Some(Token::Separator(self.iter.by_ref().collect()))},
+            // '='    => {Some(Token::Implies(self.iter.by_ref().collect()))},
             '1'...'9' => Some(Token::Atom(self.iter.by_ref().take_while(|&c| c.is_numeric()).collect())),
             'A'...'Z' => Some(Token::Atom(self.iter.by_ref().take_while(|&c| c.is_uppercase()).collect())),
             'a'...'z' => Some(Token::Atom(self.iter.by_ref().take_while(|&c| c.is_lowercase()).collect())),
-            '.' => None,
-            _ => self.next(),
+            '+'       => Some(Token::Atom(self.iter.by_ref().take(1).collect())),
+            ' '       => Some(Token::Whitespace(self.iter.by_ref().take(1).collect())),
+            '♥'       => None, // @TODO remove: unwrap_or condition
+            _         => None, // @TODO Currently ends when there is an undefined token. Research skipping.
+                               // self.iter.skip(&self);
         }
     }
 }
 
 fn main(){
-    let args: Vec<_> = env::args().collect(); //Strings?
-    // let re = Regex::new(r"\.rsc$");
-    // assert!(re.unwrap().is_match(&args[0]));
-
-    let input = "123 456".chars();
-    let tokenizer = Tokenizer::new(input);
+    let file_name = env::args().nth(1).unwrap();
+    let file = load_file(file_name);
+    let tokenizer = Tokenizer::new(file.chars());
 
     for token in tokenizer {
         println!("{:?}", token);
     }
 }
 
+fn load_file<P: AsRef<Path>>(path: P) -> String {
+    // @TODO Check filetype? Is this really necessary?
+    // let re = Regex::new(r"\.rsc$");
+    // assert!(re.unwrap().is_match(&path);
+
+    let mut file = File::open(path).unwrap();
+    let mut file_buffer = String::new();
+    file.read_to_string(&mut file_buffer).unwrap();
+    file_buffer
+}
