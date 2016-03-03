42e346682b1a8239f9724ef4296ccf98a17472a7
Bryan Mytko <bryanmytko@gmail.com>
2016-03-03 01:27:04 -0500

    Add parser patterns

--- src/parser.rs
+++ src/parser.rs
@@ -12,8 +12,38 @@ pub struct Rule {
 
 impl Parser {
     pub fn new() -> Parser {
+        let var = "([:lower:][:alnum:]*)";
+        let int = "([:digit]+)";
+        let exp = "(.*)";
+
+        let patterns = vec![
+            ("Var", vec![var]),
+            ("Integer", vec![int]),
+            ("AssignVar", vec![var, "=", exp]),
+            ("Add", vec![exp, "+", exp]),
+            ("Subtract", vec![exp, "+", exp]),
+        ];
+
         let mut rules = Vec::new();
-        rules.push(Rule { name: "Test".to_string(), pattern: Regex::new("[a-z]").unwrap() });
+
+        for pattern in patterns.iter() {
+            let name = pattern.0;
+            let ref pattern_group = pattern.1;
+
+            let mut str = String::from("^");
+            for p in pattern_group.iter() {
+                str.push_str(*p);
+            }
+            str.push_str("$");
+
+            rules.push(
+                Rule {
+                    name: String::from(name),
+                    pattern: Regex::new(&str).unwrap(),
+                }
+            )
+        }
+
         Parser { rules: rules }
     }
 }
