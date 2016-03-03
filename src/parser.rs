dd19201db1395cd0237b31eef40e94840c96e1d8
Bryan Mytko <bryanmytko@gmail.com>
2016-03-02 20:32:55 -0500

    Add Rule struct and Parser initialization

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,25 @@
-    // for token in tokenizer {
+use regex::Regex;
+
+pub struct Parser {
+    pub rules: Vec<Rule>,
+}
+
+#[derive(Debug)]
+pub struct Rule {
+    name: String,
+    pattern: Regex,
+}
+
+impl Parser {
+    pub fn new() -> Parser {
+        let mut rules = Vec::new();
+        rules.push(Rule { name: "Test".to_string(), pattern: Regex::new("[a-z]").unwrap() });
+        Parser { rules: rules }
+    }
+}
+
+
+// for token in tokenizer {
     //     match token {
     //         Token::Integer(_) => current_expression.push(token),
     //         Token::Plus(_)    => current_expression.push(token),
