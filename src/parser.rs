16562ff3ade3d306e5519a9a4be908e4ef359339
Bryan Mytko <bryanmytko@gmail.com>
2016-03-05 15:23:19 -0500

    Remove old comments / ideas

--- src/parser.rs
+++ src/parser.rs
@@ -51,19 +51,3 @@ impl Parser {
         Parser { rules: rules }
     }
 }
-
-
-// for token in tokenizer {
-    //     match token {
-    //         Token::Integer(_) => current_expression.push(token),
-    //         Token::Plus(_)    => current_expression.push(token),
-    //         Token::Minus(_)   => current_expression.push(token),
-    //         Token::Div(_)     => current_expression.push(token),
-    //         Token::Mod(_)     => current_expression.push(token),
-    //         Token::Equal(_)   => current_expression.push(token),
-    //         Token::Ln(_)      => { execute_line(&current_expression); current_expression.clear() },
-    //         _                 => println!("skip"),
-    //     }
-    //
-    //     // println!("{:?}", token);
-    // }
