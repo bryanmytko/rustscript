0195b38fcc376bd565269de14d60364cf9e63c41
Bryan Mytko <bryanmytko@gmail.com>
2016-03-05 17:21:37 -0500

    Remove take_while (bug) which was consuming extra element

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,5 @@
 use std::iter::Peekable;
+use regex::Regex;
 
 #[derive(Eq, PartialEq, Debug, Clone)]
 /* @TODO This is just random junk. Will need a real grammer eventually */
@@ -35,26 +36,33 @@ impl<I: Iterator<Item=char>> Tokenizer<I> {
 impl<I: Iterator<Item=char>> Iterator for Tokenizer<I> {
     type Item = Token;
 
+
     fn next(&mut self) -> Option<Token> {
-        // how to match these
-        // let keywords = vec!["def", "end", "if", "else", "elsif"];
+        // let re = Regex::new(r"^\s*").unwrap();
+        // if let Some((_, o)) = re.is_match("asdf"){
+        //     self.iter.by_ref().take(o);
+        // }
+
+        /* Since take_while consumes the last unmatched value it doesn't work
+           for gathering atoms / strings. (argh)
+
+           Exploring a way to do this via match. */
+
 
         match *self.iter.peek().unwrap_or(&'♥') {
-            '('       => Some(Token::ParenL(self.iter.by_ref().take(1).collect())),
-            ')'       => Some(Token::ParenR( self.iter.by_ref().take(1).collect())),
-            ','       => Some(Token::Separator(self.iter.by_ref().take(1).collect())),
-            '='       => Some(Token::Equal(self.iter.by_ref().take_while(|&c| c == '=').collect())),
-            '+'       => Some(Token::Plus(self.iter.by_ref().take(1).collect())),
-            '-'       => Some(Token::Minus(self.iter.by_ref().take(1).collect())),
-            '/'       => Some(Token::Div(self.iter.by_ref().take(1).collect())),
-            '*'       => Some(Token::Mult(self.iter.by_ref().take(1).collect())),
-            '%'       => Some(Token::Mod(self.iter.by_ref().take(1).collect())),
-            '1'...'9' => Some(Token::Integer(self.iter.by_ref().take_while(|&c| c.is_numeric()).collect())),
-            'A'...'z' => Some(Token::Atom(self.iter.by_ref().take_while(|&c| c.is_alphabetic()).collect())),
-            '\n'      => Some(Token::Ln(self.iter.by_ref().take(1).collect())),
-            ' '       => Some(Token::Whitespace(self.iter.by_ref().take(1).collect())),
-            '♥'       => None,
-            _         => None, // @TODO Do we want None on invalid syntax?
+            '(' => Some(Token::ParenL(self.iter.by_ref().take(1).collect())),
+            ')' => Some(Token::ParenR(self.iter.by_ref().take(1).collect())),
+            ',' => Some(Token::Separator(self.iter.by_ref().take(1).collect())),
+            '=' => Some(Token::Equal(self.iter.by_ref().take_while(|&c| c == '=').collect())),
+            '+' => Some(Token::Plus(self.iter.by_ref().take(1).collect())),
+            '-' => Some(Token::Minus(self.iter.by_ref().take(1).collect())),
+            '/' => Some(Token::Div(self.iter.by_ref().take(1).collect())),
+            '*' => Some(Token::Mult(self.iter.by_ref().take(1).collect())),
+            '%' => Some(Token::Mod(self.iter.by_ref().take(1).collect())),
+            '\r' | '\n' => Some(Token::Ln(self.iter.by_ref().take(1).collect())),
+            ' ' => Some(Token::Whitespace(self.iter.by_ref().take(1).collect())),
+            '♥' => None,
+            _   => None, // @TODO Do we want None on invalid syntax?
         }
     }
 }
