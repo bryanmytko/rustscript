02f4ca2f2f6a56bcd2d20b066b0716d8d0e09197
Bryan Mytko <bryanmytko@gmail.com>
2016-03-09 03:34:13 -0500

    Make lexer work (finally!) without take_while

--- src/lexer.rs
+++ src/lexer.rs
@@ -5,20 +5,20 @@ use regex::Regex;
 #[derive(Eq, PartialEq, Debug, Clone)]
 pub enum Token {
     Atom(String),
-    ParenL(String),
-    ParenR(String),
-    Separator(String),
-    Implies(String),
     Integer(String),
     Variable(String),
-    Equal(String),
-    Whitespace(String),
-    Plus(String),
-    Minus(String),
-    Div(String),
-    Mult(String),
-    Mod(String),
-    Ln(String),
+    Separator(String),
+    Implies(String),
+    Eq(String), // @TODO Need to distinguish =, ==, ===
+    ParenL,
+    ParenR,
+    Plus,
+    Minus,
+    Div,
+    Mult,
+    Mod,
+    Ln,
+    Whitespace,
 }
 
 #[derive(Clone)]
@@ -37,24 +37,30 @@ impl<I: Iterator<Item=char>> Tokenizer<I> {
         }
     }
 
-    fn parse_word(&mut self) -> Option<Token> {
-        // Need to parse until non alpha
-        // self.char_at(self.pos).to_string()
-        // let (start, end) =
-        //             match (regex!(r"^'([^']|\\')*'")).find(self.buf[self.pos..]) {
-        //                             Some((s, e)) => (s + self.pos, e + self.pos),
-        //                                         None => return None
-        //                                                     };
-        //     self.pos = end;
-        //         Some(Token::StrLit(self.buf[start + 1..end - 1]).to_string()))
+    fn match_pattern(&mut self, pattern: Regex) -> String {
         let(start, end) =
-            match (regex!(r"^'([^']|\\')*'")).find(&self.buf[self.pos..]) {
+            match pattern.find(&self.buf[self.pos..]) {
                 Some((s, e)) => (s + self.pos, e + self.pos),
-                None => { return None },
+                None => { return String::from("") },
             };
 
         self.pos = end;
-        Some(Token::Atom(self.buf[start + 1..end - 1].to_string()))
+        self.buf[start..end].to_string()
+    }
+
+    fn parse_word(&mut self) -> Option<Token> {
+        let result = self.match_pattern(Regex::new(r"[:alpha:]*").unwrap());
+        Some(Token::Atom(result))
+    }
+
+    fn parse_digit(&mut self) -> Option<Token> {
+        let result = self.match_pattern(Regex::new(r"\d*").unwrap());
+        Some(Token::Integer(result))
+    }
+
+    fn parse_eq(&mut self) -> Option<Token> {
+        let result = self.match_pattern(Regex::new(r"=*").unwrap());
+        Some(Token::Eq(result))
     }
 }
 
@@ -62,39 +68,22 @@ impl<I: Iterator<Item=char>> Iterator for Tokenizer<I> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Token> {
-        if self.iter.size_hint().1.unwrap() >= self.pos {
-            self.pos += 1;
-        } else {
+        if self.pos >= self.iter.size_hint().1.unwrap() {
             return None;
         }
 
-        println!("Position: {:?}", self.pos);
-
-        /* Need to match over something other than "next". Buffer position? */
-        // match self.iter.next().unwrap() {
         match self.buf.char_at(self.pos) {
-            '\n' => Some(Token::Ln("\n".to_string())),
-            'a'...'z' => self.parse_word(),
+            ' '  => { self.pos += 1; Some(Token::Whitespace) },
+            '\n' => { self.pos += 1; Some(Token::Ln) },
+            '+'  => { self.pos += 1; Some(Token::Plus) },
+            '-'  => { self.pos += 1; Some(Token::Minus) },
+            '/'  => { self.pos += 1; Some(Token::Div) },
+            '*'  => { self.pos += 1; Some(Token::Mult) },
+            '%'  => { self.pos += 1; Some(Token::Mod) },
+            '='  => self.parse_eq(),
+            'A'...'z' => self.parse_word(),
+            '1'...'9' => self.parse_digit(),
             _   => None,
         }
     }
 }
-    //     match *self.iter.peek().unwrap_or(&'♥') {
-    //         '(' => Some(Token::ParenL(self.iter.by_ref().take(1).collect())),
-    //         ')' => Some(Token::ParenR(self.iter.by_ref().take(1).collect())),
-    //         ',' => Some(Token::Separator(self.iter.by_ref().take(1).collect())),
-    //         '=' => Some(Token::Equal(self.iter.by_ref().take_while(|&c| c == '=').collect())),
-    //         '+' => Some(Token::Plus(self.iter.by_ref().take(1).collect())),
-    //         '-' => Some(Token::Minus(self.iter.by_ref().take(1).collect())),
-    //         '/' => Some(Token::Div(self.iter.by_ref().take(1).collect())),
-    //         '*' => Some(Token::Mult(self.iter.by_ref().take(1).collect())),
-    //         '%' => Some(Token::Mod(self.iter.by_ref().take(1).collect())),
-    //         'a'...'z' => Some(Token::Atom(self.iter.by_ref().take_while(|&c| c.is_alphabetic()).collect())),
-    //         'A'...'Z' => Some(Token::Atom(self.iter.by_ref().take_while(|&c| c.is_alphabetic()).collect())),
-    //         '1'...'9' => Some(Token::Atom(self.iter.by_ref().take_while(|&c| c.is_alphabetic()).collect())),
-    //         '\r' | '\n' => Some(Token::Ln(self.iter.by_ref().take(1).collect())),
-    //         ' ' => Some(Token::Whitespace(self.iter.by_ref().take(1).collect())),
-    //         '♥' => None,
-    //         _   => None, // @TODO Do we want None on invalid syntax?
-    //     }
-    // }
